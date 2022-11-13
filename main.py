909c628f0839c1e2b8de2fe6d0b7a337886032c1
Noah Yacoub <noahyacoub123@gmail.com>
2022-11-13 18:31:42 -0500

    First steps

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-#Initials for who did what, Edward Hong as EH, Alden Panicker as AP, and Noah Yacoub as NY -NY
+# Initials for who did what, Edward Hong as EH, Alden Panicker as AP, and Noah Yacoub as NY -NY
+import pygame  #importing the pygame module -NY
+import random  #Importing the random module -NY
+def gameboard_drawing(board):
+    #This function will 'draw' the game board as a list of 16 (4x4) instead of the usual 9 (3x3) -NY
+    print(board[13]) + '|' + print(board[14]) +'|' + print(board[15]) + '|' + print(board[16])
+    print('_______')  #Just a horizontal line to separate the rows and make it look a little cleaner -NY
+    print(board[9]) + '|' + print(board[10]) + '|' + print(board[11]) + '|' + print(board[12])
+    print('_______')
+    print(board[5]) + '|' + print(board[6]) + '|' + print(board[7]) + '|' + print(board[8])
+    print('_______')
+    print(board[1]) + '|' + print(board[2]) + '|' + print(board[3]) + '|' + print(board[4])
+
+
