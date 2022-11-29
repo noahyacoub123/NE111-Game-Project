50fc0f7baed72285cb510d337bf1983c4b812909
Noah Yacoub <noahyacoub123@gmail.com>
2022-11-29 18:52:17 -0500

    A few more functions completed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Initials for who did what, Edward Hong as EH, Alden Panicker as AP, and Noah Yacoub as NY -NY
 import pygame  #importing the pygame module -NY
-import random  #Importing the random module -NY
+import random #Importing the random module -NY
 def gameboard_drawing(board):
     #This function will 'draw' the game board as a list of 16 (4x4) instead of the usual 9 (3x3) -NY
     print(board[13]) + '|' + print(board[14]) +'|' + print(board[15]) + '|' + print(board[16])
@@ -11,4 +11,19 @@ def gameboard_drawing(board):
     print('_______')
     print(board[1]) + '|' + print(board[2]) + '|' + print(board[3]) + '|' + print(board[4])
 
+def InputPlayerLetter():
+    #This function will let the player choose which letter they want to be in the game, then choosing the computers letter based on that
+    letter = "" #Initializing the letter variable
+    while not (letter == "X" or letter == "O"):
+        print("Hello player, do you want to be X or O? ")
+        letter = input().upper() #making sure that the letter chosen will be an upper case letter for simplicity
+        if letter == "X":
+            return ["X", "O"]
+        else:
+            return ["O", "X"] #Players letter will be shown as the first index and the computer will be the second
 
+def getComputerMove(board, computerLetter):
+    if computerLetter == "X":
+        playerLetter == "O"
+    else:
+        playerLetter == "X"
