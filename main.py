728b28965055e515481d64e8f561f28db12db107
Noah Yacoub <noahyacoub123@gmail.com>
2022-11-29 19:27:13 -0500

    Fixing

--- main.py
+++ main.py
@@ -11,4 +11,37 @@ def gameboard_drawing(board):
     print('_______')
     print(board[1]) + '|' + print(board[2]) + '|' + print(board[3]) + '|' + print(board[4])
 
+    def getPlayerMove(board):
+        move = ''
+        while move not in '1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16'.split() or not isSpaceFree(board, int(move)):
+            print("What is your next move? (1-16)")
+            move = input()
+        return int(move)
 
+    def chooseRandomMoveFromList(board, movesList):
+        possibleMoves = []
+        for i in movesList:
+            if isSpaceFree(board, i):
+                possibleMoves.append(i)
+
+        if len(possibleMoves) != 0:
+            return random.choice(possibleMoves)
+        else:
+            return None
+
+def InputPlayerLetter():
+    #This function will let the player choose which letter they want to be in the game, then choosing the computers letter based on that -NY
+    letter = "" #Initializing the letter variable
+    while not (letter == "X" or letter == "O"):
+        print("Hello player, do you want to be X or O? ")
+        letter = input().upper() #making sure that the letter chosen will be an upper case letter for simplicity -NY
+        if letter == "X":
+            return ["X", "O"]
+        else:
+            return ["O", "X"] #Players letter will be shown as the first index and the computer will be the second -NY
+
+def getComputerMove(board, computerLetter): #Verifies that the choice of letter of the player and computer will be opposite -NY
+    if computerLetter == "X":
+        playerLetter == "O"
+    else:
+        playerLetter == "X"
