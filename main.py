918a2fc4c89ddbc1250c37581d57750bbca42aea
alden <panickeralden@gmail.com>
2022-11-29 20:58:05 -0500

    1

--- main.py
+++ main.py
@@ -30,23 +30,23 @@ def getComputerMove(board, computerLetter):
     else:
         playerLetter == "X"
 
-        def InputPlayerLetter():
-            # This function will let the player choose which letter they want to be in the game, then choosing the computers letter based on that
-            letter = ""  # Initializing the letter variable
-            while not (letter == "X" or letter == "O"):
-                print("Hello player, do you want to be X or O? ")
-                letter = input().upper()  # making sure that the letter chosen will be an upper case letter for simplicity
-                if letter == "X":
-                    return ["X", "O"]
-                else:
-                    return ["O",
-                            "X"]  # Players letter will be shown as the first index and the computer will be the second
+def InputPlayerLetter():
+    # This function will let the player choose which letter they want to be in the game, then choosing the computers letter based on that
+    letter = ""  # Initializing the letter variable
+    while not (letter == "X" or letter == "O"):
+        print("Hello player, do you want to be X or O? ")
+        letter = input().upper()  # making sure that the letter chosen will be an upper case letter for simplicity
+        if letter == "X":
+            return ["X", "O"]
+        else:
+            return ["O",
+                    "X"]  # Players letter will be shown as the first index and the computer will be the second
 
-        def getComputerMove(board, computerLetter): #Verifies that the players chosen letter will be the opposite of the computer -NY
-            if computerLetter == "X":
-                playerLetter == "O"
-            else:
-                playerLetter == "X"
+def getComputerMove(board, computerLetter): #Verifies that the players chosen letter will be the opposite of the computer -NY
+    if computerLetter == "X":
+        playerLetter == "O"
+    else:
+        playerLetter == "X"
 
 def getPlayerMove(board):
     move = ''
@@ -122,7 +122,7 @@ while True:
             if isWinner(theBoard, computerLetter):
                  drawBoard(theBoard)
                  print("The computer wins! Better luck next time!")
-                gameIsPlaying = False
+                 gameIsPlaying = False
             else:
                 turn = 'player'
     print("Do you want to play again? (yes or no)")
