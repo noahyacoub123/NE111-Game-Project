9d7a69e6773e77b0dd418cde8f8c23311596137b
alden <panickeralden@gmail.com>
2022-12-06 17:52:09 -0500

    Comments

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def makeMove(board, letter, move):
 
 def getPlayerMove(board):
     move = ''
-    while move not in '1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16'.split() or not isSpaceFree(board,int(move)):
+    while move not in '1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16'.split() or not isSpaceFree(board, int(move)):
         print("What is your next move? (1-16)")
         move = input()
     return int(move)
